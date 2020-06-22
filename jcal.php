97c3834da4006c3d69b8eaba4938e6833a1efde1
MasoudNiki <masoud.niki79@gmail.com>
2020-06-23 00:41:14 +0430

    start

---
+++ jcal.php
@@ -0,0 +1,131 @@
+<?php
+    if(file_exists('jdf.php'))
+    {
+        require_once("jdf.php");
+    }
+    else{
+        $url=
+        $start = curl_init();
+        curl_setopt($start, CURLOPT_URL, $url);
+        curl_setopt($start, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($start, CURLOPT_SSLVERSION, 3);
+        $file_data = curl_exec($start);
+
+    }
+   
+    
+    function getFirstDay(){
+
+        $current_jdate = jdate('Y/m/d', '', '', 'Asia/Tehran', 'en');
+        $arr_parts = explode('/', $current_jdate);
+        $jYear  = $arr_parts[0];
+        $jMonth = $arr_parts[1];
+        $jDay   = 1;
+        $First_Day_gdate = jalali_to_gregorian($jYear, $jMonth, $jDay, '/');
+        $First_Day_gdate_timeStamp=strtotime($First_Day_gdate);
+        return @jdate('w', $First_Day_gdate, '', 'Asia/Tehran', 'en');
+
+    }
+    function getLastDay(){
+
+        $current_jdate = jdate('Y/m/d', '', '', 'Asia/Tehran', 'en');
+        $arr_parts = explode('/', $current_jdate);
+        $jYear  = $arr_parts[0];
+        $jMonth = $arr_parts[1];
+        $jDay   = jdate('t', '', '', 'Asia/Tehran', 'en');
+        $Last_Day_gdate = jalali_to_gregorian($jYear, $jMonth, $jDay, '/');
+        $Last_Day_gdate_timeStamp=strtotime($Last_Day_gdate);
+        return @jdate('w', $Last_Day_gdate_timeStamp, '', 'Asia/Tehran', 'en');
+
+    }
+
+    function create_array(){
+
+        $FirstDay=getFirstDay();
+        $LastDay=abs(getLastDay());
+        $Days=jdate('t', '', '', 'Asia/Tehran', 'en');
+        $Day=jdate('d', '', '', 'Asia/Tehran', 'en');
+        $jMonth=jdate('m', '', '', 'Asia/Tehran', 'en');
+        $CALC=['sh','ye','do','se','ch','pa','jo'];
+        for($i=0;$i<$FirstDay;++$i){
+            $CALC[]=" "." ";
+        }
+        for($i=1;$i<=$Days;++$i)
+        {
+            if($i==$Day)
+            {
+                if($i<10)
+                {
+                    $CALC[]=" "."\033[32m".$i."\033[37m";
+                }
+                else{
+                    $CALC[]="\033[32m".$i."\033[37m";;
+                }
+                
+                
+            }
+            else{
+                if($i<10)
+                {
+                    $CALC[]=" ".$i;
+                }
+                else{
+                    $CALC[]=$i;
+                }
+
+            }
+           
+            
+           
+        }
+        for($i=$LastDay;$i<6;++$i)
+        {
+
+
+            $CALC[]=" "." ";
+        }
+        return $CALC;
+
+    }
+    function PrintCalc($CALC){
+        MonthAndYearText();
+
+        for($i=1;$i<count($CALC);++$i)
+        {
+            if(($i)%7==0)
+            {
+                echo "\033[0;31m".$CALC[$i-1]."\033[0;37m";
+                echo "\n";
+            }
+            else{
+                echo $CALC[$i-1];
+                echo " "." ";
+            }
+        }
+
+    }
+    function MonthAndYearText(){
+        $month_year_text=jdate("F-Y", '', '', 'Asia/Tehran', 'en');
+        $len=strlen($month_year_text);
+        for($i=0;$i<(26-$len)/2;++$i)
+        {
+            echo " ";
+        }
+        echo $month_year_text;
+        for($i=0;$i<(26-$len)/2;++$i)
+        {
+            echo " "; 
+        }
+        echo "\n";    }
+    
+    PrintCalc(create_array());
+
+
+
+
+
+
+
+
+
+?>
